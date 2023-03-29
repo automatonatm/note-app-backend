379c8e9e130c414feef6c0653e080bc579d50d0f
Automaton <46001539+automatonatm@users.noreply.github.com>
2023-03-29 01:43:04 +0100

    initial project setup

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express()
+
+const port = 5000
+
+
+
+app.get("/", (req, res) => {
+  res.send("OK.io")
+})
+
+
+
+
+app.listen(port, () => {
+
+  console.log(`Server running on port: ${port}`);
+
+})
